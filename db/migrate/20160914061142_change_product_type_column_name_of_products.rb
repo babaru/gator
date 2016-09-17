06f5eb42ea9e69b53ebd99e0302aefd7361b5efa
babaru <babaru.trinit@gmail.com>
2016-09-17 12:05:28 +0800

    fix product column rename bug

--- db/migrate/20160914061142_change_product_type_column_name_of_products.rb
+++ db/migrate/20160914061142_change_product_type_column_name_of_products.rb
@@ -1,11 +1,11 @@
 class ChangeProductTypeColumnNameOfProducts < ActiveRecord::Migration
   def up
-    rename_column :products, :category, :category
-    rename_column :products, :rd_category, :rd_category
+    rename_column :products, :product_type, :category
+    rename_column :products, :product_dev_type, :rd_category
   end
 
   def down
-    rename_column :products, :category, :category
-    rename_column :products, :rd_category, :rd_category
+    rename_column :products, :category, :product_type
+    rename_column :products, :rd_category, :product_dev_type
   end
 end
